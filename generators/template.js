856ad9293a7ea5fa837379493aab643661759572
CuteMeiyu <151729943+CuteMeiyu@users.noreply.github.com>
2024-06-06 16:50:10 +0800

    Update template.js

--- generators/template.js
+++ generators/template.js
@@ -1,4 +1,4 @@
-(function(root, factory) {
+(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD环境
         define([], factory);
@@ -9,7 +9,18 @@
         // 浏览器全局环境
         root.RankedGenerator = factory();
     }
-}(this, function() {
+}(this, function () {
     var Generator = {};
+    Generator.generate = generate;
+    
+    function generate(goalPool, settings) {
+        return [
+            0, 1, 2, 3, 4,
+            5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14,
+            15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24
+        ];
+    }
     return Generator;
 }));
