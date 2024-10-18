4adbd22b91b30a7287a5788e0be6c1ab5e29c2ed
CuteMeiyu <151729943+CuteMeiyu@users.noreply.github.com>
2024-10-18 17:19:18 +0800

    add HSR goals

--- data-converter.py
+++ data-converter.py
@@ -15,7 +15,10 @@ def prune(df: pd.DataFrame):
         if pd.isna(name) or not contains_chinese(name) or pd.isna(row["rank"]):
             df2.drop(index, inplace=True)
             continue
-    df2.drop("hidden notes", axis=1, inplace=True)
+    if "hidden notes" in df.columns:
+        df2.drop("hidden notes", axis=1, inplace=True)
+    if "comments" in df.columns:
+        df2.drop("comments", axis=1, inplace=True)
     return df2
 
 
@@ -24,6 +27,7 @@ def main():
         [
             prune(pd.read_excel("bingo.xlsx", sheet_name="7-11任务表")),
             prune(pd.read_excel("bingo.xlsx", sheet_name="1-6任务表")),
+            prune(pd.read_excel("bingo.xlsx", sheet_name="HSR任务表")),
         ]
     )
     df.to_csv("data.csv", index=False)
